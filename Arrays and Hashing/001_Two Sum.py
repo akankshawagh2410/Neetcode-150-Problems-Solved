a473bb68a98bba762fb90e928e51c9d542f1f83a
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-10-11 17:31:59 -0700

    Update 001_Two Sum.py

--- Arrays and Hashing/001_Two Sum.py
+++ Arrays and Hashing/001_Two Sum.py
@@ -1,7 +1,9 @@
 # Leetcode Problem 1: Two Sum
-
-# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. You may assume that every input has exactly one pair of indices i and j that satisfy the condition. Return the answer with the smaller index first.
-
+'''
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. 
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition. 
+Return the answer with the smaller index first.
+'''
 # Code Solution: 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
