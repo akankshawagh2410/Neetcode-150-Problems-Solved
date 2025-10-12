298c5f00320af821ae9b2d5160e57b0d56991216
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-10-11 17:27:44 -0700

    Update 242_Valid Anagram.py

--- Arrays and Hashing/242_Valid Anagram.py
+++ Arrays and Hashing/242_Valid Anagram.py
@@ -1,7 +1,8 @@
-# Leetcode Problem 242: Valid Anagram
-
-# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false. An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
-
+#Leetcode Problem 242: Valid Anagram
+'''
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false. 
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+'''
 # My Code Solution: 17ms
 
 class Solution:
