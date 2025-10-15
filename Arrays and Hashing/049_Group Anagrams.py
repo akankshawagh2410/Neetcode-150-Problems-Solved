b95a8a81e5e616dde14ac010febdc873eb688f41
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-10-15 14:45:16 -0700

    Create 049_Group Anagrams.py
    49 solution added

---
+++ Arrays and Hashing/049_Group Anagrams.py
@@ -0,0 +1,20 @@
+# Leetcode Problem 49: Group Anagrams
+'''
+Given an array of strings strs, group all anagrams together into sublists. 
+You may return the output in any order. An anagram is a string that contains the exact 
+same characters as another string, but the order of the characters can be different.
+'''
+# Code Solution: 
+
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        return list(anagram_map.values())
+
+# Link to the code explanation:
