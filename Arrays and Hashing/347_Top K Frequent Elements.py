99b053fa4ecbb2aeed367f9eaf1f4dfeb85460a7
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-10-20 14:52:08 -0700

    Create 347_Top K Frequent Elements.py
    347 added

---
+++ Arrays and Hashing/347_Top K Frequent Elements.py
@@ -0,0 +1,17 @@
+# Leetcode Problem 347: Top K Frequent Elements
+'''
+Given an integer array nums and an integer k, return the k most frequent elements within the array.
+The test cases are generated such that the answer is always unique.
+You may return the output in any order.
+'''
+# Code Solution: 
+from collections import Counter
+from heapq import nlargest
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_map = Counter(nums)
+        return [num for num, _ in nlargest(k, freq_map.items(), key=lambda x: x[1])]
+
+# Link to the code explanation:
