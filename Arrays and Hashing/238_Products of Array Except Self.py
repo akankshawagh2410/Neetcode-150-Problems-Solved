53bd8b59b7292d8ea53bfc9fedebb562f116b49d
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-11-11 15:32:51 -0800

    Create 238_Products of Array Except Self.py
    code updated

---
+++ Arrays and Hashing/238_Products of Array Except Self.py
@@ -0,0 +1,27 @@
+# Leetcode Problem 238: Products of Array Except Self
+'''
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+Each product is guaranteed to fit in a 32-bit integer.
+Follow-up: Could you solve it in O(n) time without using the division operation?
+'''
+# Code Solution: 
+
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output = [1] * n
+
+        left_product = 1
+        for i in range(n):
+            output[i] = left_product  
+            left_product *= nums[i]   
+        
+        right_product = 1
+        for i in range(n - 1, -1, -1): 
+            output[i] *= right_product
+            right_product *= nums[i]
+            
+        return output
+# Link to the code explanation:
