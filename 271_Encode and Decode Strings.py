9cd70370899577e97d3d79c1d1329f9178941879
Akanksha Wagh <87199236+akankshawagh2410@users.noreply.github.com>
2025-11-12 12:02:27 -0800

    Create 271_Encode and Decode Strings.py
    code updated

---
+++ 271_Encode and Decode Strings.py
@@ -0,0 +1,28 @@
+# Leetcode Problem 271: Encode and Decode Strings
+'''
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+Please implement encode and decode
+'''
+# Code Solution: 
+from typing import List
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            decoded.append(s[j+1:j+1+length])
+            i = j + 1 + length
+        return decoded
+
+# Link to the code explanation:
